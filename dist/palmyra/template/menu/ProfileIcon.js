679d2d883d44261b6167a6cc2b07e05a9e81de40
Agasthiya1803 <cs1803agasthiyar@gmail.com>
2024-10-24 16:42:27 +0530

    latest build

--- dist/palmyra/template/menu/ProfileIcon.js
+++ dist/palmyra/template/menu/ProfileIcon.js
@@ -1,17 +1,17 @@
-import { jsxs as r, jsx as e } from "react/jsx-runtime";
-import { Popover as i, Button as o } from "@mantine/core";
-const c = (l) => /* @__PURE__ */ r("div", { className: "profile-menu", children: [
+import { jsxs as i, jsx as e } from "react/jsx-runtime";
+import { Popover as r, Button as l } from "@mantine/core";
+const c = (n) => /* @__PURE__ */ i("div", { className: "profile-menu", children: [
   " ",
-  /* @__PURE__ */ r(i, { width: 200, position: "bottom", withArrow: !0, shadow: "md", children: [
-    /* @__PURE__ */ e(i.Target, { children: /* @__PURE__ */ r(o, { children: [
+  /* @__PURE__ */ i(r, { width: 200, position: "bottom", withArrow: !0, children: [
+    /* @__PURE__ */ e(r.Target, { children: /* @__PURE__ */ i(l, { variant: "transparent", size: "compact-md", children: [
       "Welcome ",
-      l.displayName
+      n.displayName
     ] }) }),
-    /* @__PURE__ */ e(i.Dropdown, { children: /* @__PURE__ */ r("ul", { children: [
+    /* @__PURE__ */ e(r.Dropdown, { children: /* @__PURE__ */ e("div", { className: "profile-menu-list-container", children: /* @__PURE__ */ i("ul", { children: [
       /* @__PURE__ */ e("li", { children: /* @__PURE__ */ e("span", { className: "profile-sub-menu", children: "Profile" }) }),
       /* @__PURE__ */ e("li", { children: /* @__PURE__ */ e("span", { className: "profile-sub-menu", children: "Settings" }) }),
       /* @__PURE__ */ e("li", { children: /* @__PURE__ */ e("span", { className: "profile-sub-menu", children: "Log out" }) })
-    ] }) })
+    ] }) }) })
   ] })
 ] });
 export {
