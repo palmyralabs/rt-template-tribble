3b839f79d6dcdc6c5f6d6ee79e90dc28e88b1327
Raja K <ksvraja@gmail.com>
2024-12-27 23:35:14 +0530

    build file push

--- dist/palmyra/template/form/SummaryGrid.js
+++ dist/palmyra/template/form/SummaryGrid.js
@@ -1,32 +1,33 @@
-import { jsx as o } from "react/jsx-runtime";
-import { useRef as u } from "react";
-import { SummaryGridControls as g } from "./SummaryGridControls.js";
+import { jsx as n } from "react/jsx-runtime";
+import { useRef as s } from "react";
+import { SummaryGridControls as u } from "./SummaryGridControls.js";
 import { useNavigate as f } from "react-router-dom";
 import { StringFormat as P } from "@palmyralabs/ts-utils";
 import '../../../assets/Layout.css';/* empty css                     */
 import { PalmyraGrid as C } from "@palmyralabs/rt-forms-mantine";
 import { getTitle as k } from "../util/TitleUtil.js";
 function O(i) {
-  const t = f(), n = i.idKey || "id", e = i.gridRef || u(null), r = (m) => {
-    const l = { id: m[n] }, s = i.clickTo || "view";
-    t(P(s + "/{id}", l));
-  }, a = () => {
+  const t = f(), o = i.idKey || "id", e = i.gridRef || s(null), a = (m) => {
+    const l = { id: m[o] }, g = i.clickTo || "view";
+    t(P(g + "/{id}", l));
+  }, r = () => {
     t("new");
-  }, c = i.DataGridControls || g, d = i.disableRowClick ? () => {
-  } : r;
-  return /* @__PURE__ */ o("div", { className: "py-grid-container", children: /* @__PURE__ */ o(
+  }, c = i.DataGridControls || u, d = i.disableRowClick ? () => {
+  } : a;
+  return /* @__PURE__ */ n("div", { className: "py-grid-container", children: /* @__PURE__ */ n(
     C,
     {
       title: k(i.title, "grid"),
       columns: i.columns,
+      pagination: i.pagination,
+      pageSize: i.pageSize,
       getPluginOptions: i.getPluginOptions,
       defaultParams: i.defaultParams,
       DataGridControls: c,
-      DataGridControlProps: { newRecord: a },
+      DataGridControlProps: { newRecord: r },
       endPoint: i.options.endPoint,
       endPointOptions: i.options.endPointOptions,
       onRowClick: d,
-      pageSize: i.pageSize,
       ...i.options,
       ref: e,
       customizer: i.customizer,
--- dist/palmyra/template/popup/SummaryPopupGrid.js
+++ dist/palmyra/template/popup/SummaryPopupGrid.js
@@ -1,6 +1,6 @@
 import { jsxs as C, jsx as m } from "react/jsx-runtime";
 import { useRef as s, useEffect as D } from "react";
-import { topic as r } from "@palmyralabs/ts-utils";
+import { topic as i } from "@palmyralabs/ts-utils";
 import { PalmyraGrid as v } from "@palmyralabs/rt-forms-mantine";
 import { SummaryDialogForm as N } from "./SummaryDialogForm.js";
 import { SummaryDrawerForm as R } from "./SummaryDrawerForm.js";
@@ -8,23 +8,23 @@ import { PopupGridControls as S } from "./PopupGridControls.js";
 import '../../../assets/Layout.css';/* empty css                     */
 import { getTitle as k } from "../util/TitleUtil.js";
 function j(e) {
-  const u = e.pageName + "/viewPage", l = e.pageName + "/newPage", d = e.pageName + "/refresh", f = e.popup || "drawer", n = s(), t = e.gridRef || s(null);
+  const u = e.pageName + "/viewPage", l = e.pageName + "/newPage", d = e.pageName + "/refresh", f = e.popup || "drawer", n = s(), r = e.gridRef || s(null);
   D(() => {
-    var i = r.subscribe(u, (c, a) => {
+    var t = i.subscribe(u, (c, a) => {
       o(a);
-    }), p = r.subscribe(d, (c) => {
-      t.current && t.current.refresh();
-    }), h = r.subscribe(l, (c, a) => {
+    }), p = i.subscribe(d, (c) => {
+      r.current && r.current.refresh();
+    }), h = i.subscribe(l, (c, a) => {
       o(a);
     });
     return () => {
-      r.unsubscribe(i), r.unsubscribe(h), r.unsubscribe(p);
+      i.unsubscribe(t), i.unsubscribe(h), i.unsubscribe(p);
     };
   }, []);
-  const g = (i) => {
-    o(i);
-  }, o = (i) => {
-    n.current && n.current.setData(i);
+  const g = (t) => {
+    o(t);
+  }, o = (t) => {
+    n.current && n.current.setData(t);
   }, P = e.DataGridControls || S, b = f == "drawer" ? R : N, w = e.disableRowClick ? () => {
   } : g;
   return /* @__PURE__ */ C("div", { className: "py-grid-container", children: [
@@ -34,6 +34,7 @@ function j(e) {
         title: k(e.title, "grid"),
         columns: e.columns,
         DataGridControlProps: { setFormData: o },
+        pagination: e.pagination,
         DataGridControls: P,
         onRowClick: w,
         defaultParams: e.defaultParams,
@@ -42,12 +43,12 @@ function j(e) {
         pageSize: e.pageSize,
         ...e.options,
         getPluginOptions: e.getPluginOptions,
-        ref: t,
+        ref: r,
         customizer: e.customizer,
         quickSearch: e.quickSearch
       }
     ),
-    /* @__PURE__ */ m(b, { ...e, gridRef: t, ref: n })
+    /* @__PURE__ */ m(b, { ...e, gridRef: r, ref: n })
   ] });
 }
 export {
